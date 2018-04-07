7357814dedec12f2297007ef81001ccc00f469a0
kaicheng107 <kaicheng107@hotmail.com>
2018-04-07 18:32:56 +0800

    Update basic admin function

---
+++ votingNetwork/votingNetwork/adminNetwork/src/java/jsf/managedbean/UpdateNricManagedBean.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsf.managedbean;
+
+import ebj.session.stateless.AdminSessionBeanLocal;
+import entity.Voter;
+import enumeration.VoterState;
+import exception.VoterNotFoundException;
+import java.io.IOException;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+/**
+ *
+ * @author KaiCheng
+ */
+@Named(value = "updateNricManagedBean")
+@RequestScoped
+public class UpdateNricManagedBean {
+
+    @EJB(name = "AdminSessionBeanLocal")
+    private AdminSessionBeanLocal adminSessionBeanLocal;
+    
+    /**
+     * Creates a new instance of UpdateNricManagedBean
+     */
+    private Boolean notVoted;
+    private Voter voter;
+    public UpdateNricManagedBean() {
+        voter = (Voter) FacesContext.getCurrentInstance().getExternalContext().getFlash().get("voter");
+        System.err.println("*********Getting NRIC***********"+voter.getNric()+"*******"+voter.getVoteState().toString());
+        FacesContext.getCurrentInstance().getExternalContext().getFlash().put("voter", voter);
+        if(voter.getVoteState().equals(VoterState.ELIGIBLE)){
+            setNotVoted((Boolean) true);
+        }else{
+            setNotVoted((Boolean) false);
+        }
+    }
+    
+    public void updateNric(ActionEvent event)throws IOException{
+        System.err.println("*********Update NRIC***********"+voter.getNric());
+        try{
+            
+        adminSessionBeanLocal.updateVoterStatus((Voter) FacesContext.getCurrentInstance().getExternalContext().getFlash().get("voter"));
+        FacesContext.getCurrentInstance().getExternalContext().redirect("checkNric.xhtml");
+        }catch(VoterNotFoundException ex){
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Invalid NRIC: " + ex.getMessage(), null)); 
+        }
+    }
+    
+    public void back(ActionEvent event)throws IOException{
+        FacesContext.getCurrentInstance().getExternalContext().redirect("checkNric.xhtml");
+    }
+    /**
+     * @return the notVoted
+     */
+    public Boolean getNotVoted() {
+        return notVoted;
+    }
+
+    /**
+     * @param notVoted the notVoted to set
+     */
+    public void setNotVoted(Boolean notVoted) {
+        this.notVoted = notVoted;
+    }
+
+    /**
+     * @return the voter
+     */
+    public Voter getVoter() {
+        return voter;
+    }
+
+    /**
+     * @param voter the voter to set
+     */
+    public void setVoter(Voter voter) {
+        this.voter = voter;
+    }
+    
+    
+}
